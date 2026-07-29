00bd59af71a80e51165e5c0b879c55c4f7199d4e
Gael83OK <gaelberlinger@hotmail.com>
2026-07-29 13:39:00 +0200

    Améliorations mobile

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,5 +1,4 @@
 import { BottomNav } from "@/components/layout/BottomNav";
-import { BrandLogo } from "@/components/brand";
 
 interface AppLayoutProps {
   children: React.ReactNode;
@@ -8,11 +7,6 @@ interface AppLayoutProps {
 export function AppLayout({ children }: AppLayoutProps) {
   return (
     <div className="mx-auto min-h-screen max-w-lg bg-offwhite">
-      {/* Logo Artefacts */}
-      <header className="flex justify-center px-5 pt-6">
-        <BrandLogo size="md" priority />
-      </header>
-
       {/* Contenu principal */}
       <main className="px-5 pt-6 pb-16 safe-top">
         {children}
